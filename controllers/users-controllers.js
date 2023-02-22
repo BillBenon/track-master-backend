ab2f0f5892d82ac9c8c90e4746a3fba5f5f9922e
Dushime Bill Benon <DushimeBillBenon@gmail.com>
2023-02-22 23:43:50 +0200

    feat: users controllers finished

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -60,37 +60,28 @@ exports.signUp = async (req, res, next) => {
         return next(
             new Error(
                 errors.array()[0].msg ||
-                "Invalid inputs passed, please check your data",
+                'Invalid inputs passed, please check your data',
                 422
             )
         );
     }
 
-    const {
-        fullnames,
-        username,
-        email,
-        gender,
-        residence,
-        phone,
-        password,
-        role,
-    } = req.body;
+    const { email, password } = req.body;
 
     let existingUser;
     try {
-        existingUser = await User.findOne({ email: email });
+        [existingUser] = await db.execute('SELECT * FROM users WHERE email = ?', [email]);
     } catch (err) {
-        const error = new HttpError(
-            "Registering user failed, please try again later.",
+        const error = new Error(
+            'Registering user failed, please try again later.',
             500
         );
         return next(error);
     }
 
-    if (existingUser) {
-        const error = new HttpError(
-            "User exists already, register another user instead.",
+    if (existingUser.length > 0) {
+        const error = new Error(
+            'User exists already, register another user instead.',
             422
         );
         return next(error);
@@ -100,91 +91,27 @@ exports.signUp = async (req, res, next) => {
     try {
         hashedPassword = await bcrypt.hash(password, 12);
     } catch (err) {
-        const error = new HttpError(
-            "Could not register user, please try again.",
+        const error = new Error(
+            'Could not register user, please try again.',
             500
         );
         return next(error);
     }
-
-    const createdUser = new User({
-        fullnames,
-        username,
-        email,
-        gender,
-        residence,
-        phone,
-        role,
-        image: req.file.path,
-        password: hashedPassword,
-        blogs: [],
-    });
 
     try {
-        await createdUser.save().then(() => {
-            group
-                .find({ group_name: req.body.role })
-                .then((group1) => {
-                    if (group1.length == 0) {
-                        let createGroup = new group({
-                            group_name: req.body.role,
-                            members: createdUser.id,
-                        });
-                        createGroup
-                            .save()
-                            .then((info) => {
-                                return res
-                                    .statu(201)
-                                    .json({ userId: createdUser.id, email: createdUser.email });
-                            })
-                            .catch((err) => {
-                                return next(
-                                    new HttpError(
-                                        "Creating new user group failed, try again!",
-                                        500
-                                    )
-                                );
-                            });
-                    } else {
-                        group
-                            .findOneAndUpdate(
-                                { group_name: req.body.role },
-                                {
-                                    $push: { members: createdUser.id },
-                                }
-                            )
-                            .then(() => {
-                                return res
-                                    .status(201)
-                                    .json({ userId: createdUser.id, email: createdUser.email });
-                            })
-                            .catch((err) => {
-                                return next(
-                                    new HttpError(
-                                        `Putting user in ${req.body.role}'s group failed, try again!`,
-                                        500
-                                    )
-                                );
-                            });
-                    }
-                })
-                .catch((err) => {
-                    const error = new HttpError(
-                        "Creating user group failed, try again!",
-                        500
-                    );
-                    return next(error);
-                });
-        });
+        const [result] = await db.execute(
+            'INSERT INTO users (email, password) VALUES (?, ?)',
+            [email, hashedPassword]
+        );
+        res.status(201).json({ userId: result.insertId, email: email });
     } catch (err) {
         console.log(err);
-        const error = new HttpError(
-            "Registering user failed, please try again",
+        const error = new Error(
+            'Registering user failed, please try again',
             500
         );
         return next(error);
     }
-    // res.status(201).json({ userId: createdUser.id, email: createdUser.email });
 };
 
 exports.login = async (req, res, next) => {
@@ -197,7 +124,7 @@ exports.login = async (req, res, next) => {
     let existingUser;
 
     try {
-        existingUser = await User.findOne({ email: email });
+        [existingUser] = await db.execute('SELECT * FROM users WHERE email = ?', [email]);
     } catch (err) {
         const error = new HttpError(
             "Logging in failed, please try again later.",
@@ -206,7 +133,7 @@ exports.login = async (req, res, next) => {
         next(error);
     }
 
-    if (!existingUser) {
+    if (!existingUser || existingUser.length === 0) {
         const error = new HttpError(
             "Invalid credentials, could not log you in.",
             403
@@ -216,7 +143,7 @@ exports.login = async (req, res, next) => {
 
     let isValidPassword = false;
     try {
-        isValidPassword = await bcrypt.compare(password, existingUser.password);
+        isValidPassword = await bcrypt.compare(password, existingUser[0].password);
     } catch (err) {
         const error = new HttpError("Could not log you in, please try again.", 500);
         return next(error);
@@ -234,9 +161,8 @@ exports.login = async (req, res, next) => {
     try {
         token = jwt.sign(
             {
-                userId: existingUser._id,
-                email: existingUser.email,
-                role: existingUser.role,
+                userId: existingUser[0].id,
+                email: existingUser[0].email,
             },
             process.env.JWT_KEY,
             { expiresIn: "1h" }
@@ -250,8 +176,8 @@ exports.login = async (req, res, next) => {
     }
 
     res.json({
-        userId: existingUser.id,
-        email: existingUser.email,
+        userId: existingUser[0].id,
+        email: existingUser[0].email,
         token,
     });
 };
@@ -267,44 +193,22 @@ exports.updateUser = async (req, res, next) => {
             )
         );
     }
-    const {
-        fullnames,
-        username,
-        email,
-        gender,
-        residence,
-        phoneNumber,
-        password,
-        role,
-    } = req.body;
-    let imagePath = req.body.image;
+    const { email, password } = req.body;
     const userId = req.params.uid;
-
-    if (req.file) {
-        imagePath = req.file.path.replace("\\", "/");
-    }
-
-    if (!imagePath) {
-        return next(new HttpError("No image picked.", 422));
-    }
 
     let user;
     try {
-        user = await User.findById(userId);
-        if (!user) {
+        const [rows, fields] = await db.execute('SELECT * FROM users WHERE id = ?', [userId]);
+        if (rows.length === 0) {
             return next(new HttpError("Could not find user.", 404));
         }
+        user = rows[0];
     } catch (err) {
         const error = new HttpError(
             "Something went wrong, could not update user.",
             500
         );
         return next(error);
-    }
-    if (imagePath !== user.image) {
-        fs.unlink(user.image, (err) => {
-            console.log(err);
-        });
     }
 
     let hashedPassword;
@@ -318,18 +222,8 @@ exports.updateUser = async (req, res, next) => {
         return next(error);
     }
 
-    user.fullnames = fullnames;
-    user.username = username;
-    user.email = email;
-    user.gender = gender;
-    user.residence = residence;
-    user.phoneNumber = phoneNumber;
-    user.password = hashedPassword;
-    user.role = role;
-    user.image = imagePath;
-
     try {
-        await user.save();
+        await db.execute('UPDATE users SET email = ?, password = ? WHERE id = ?', [email, hashedPassword, userId]);
     } catch (err) {
         const error = new HttpError(
             "Something went wrong, could not update user.",
@@ -338,55 +232,8 @@ exports.updateUser = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(200).json({ user: user.toObject({ getters: true }) });
-};
+    user.email = email;
+    user.password = hashedPassword;
 
-exports.deleteUser = async (req, res, next) => {
-    const userId = req.params.pid;
-
-    let user;
-    try {
-        user = await User.findById(userId);
-    } catch (err) {
-        const error = new HttpError(
-            "Something went wrong, could not delete user.",
-            500
-        );
-        return next(error);
-    }
-
-    if (!user) {
-        const error = new HttpError("Could not find user for this id", 404);
-        return next(error);
-    }
-
-    if (user.role == "superadmin") {
-        const error = new HttpError(
-            "You're not allowed to perform this action.",
-            403
-        );
-        return next(error);
-    }
-
-    const imagePath = user.image;
-
-    try {
-        const sess = await mongoose.startSession();
-        sess.startTransaction();
-        await user.remove({ session: sess });
-        await user.creator.save({ session: sess });
-        await sess.commitTransaction();
-    } catch (err) {
-        const error = new HttpError(
-            "Something went wrong, could not delete user.",
-            500
-        );
-        return next(error);
-    }
-
-    fs.unlink(imagePath, (err) => {
-        console.log(err);
-    });
-
-    res.status(200).json({ message: "Deleted user." });
+    res.status(200).json({ user });
 };
