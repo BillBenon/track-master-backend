89645627b932cd8304f92c71be79b684452c8c3e
ishimwe-emma <ishimweemmanuel2005@gmail.com>
2023-02-28 22:09:21 +0200

    [feat]: updated the data model and its controller

--- controllers/data-controllers.js
+++ controllers/data-controllers.js
@@ -4,92 +4,144 @@ const HttpError = require("../models/http-error");
 const Data = require("../models/Data");
 
 exports.getDataById = async (req, res, next) => {
-    const dataId = req.params.did;
+  const dataId = req.params.did;
 
-    let data;
-    try {
-        data = await Data.findByPk(dataId);
-        if (!data) {
-            throw new Error("Could not find data.");
-        }
-    } catch (err) {
-        const error = new HttpError(
-            "Something went wrong, could not find data",
-            500
-        );
-        return next(error);
+  let data;
+  try {
+    data = await Data.findByPk(dataId);
+    if (!data) {
+      throw new Error("Could not find data.");
     }
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not find data",
+      500
+    );
+    return next(error);
+  }
 
-    res.json({ data: data.toJSON() });
+  res.json({ data: data.toJSON() });
 };
 
 exports.getData = async (req, res, next) => {
-    try {
-        const data = await Data.findAll();
-        res.json({ data });
-    } catch (err) {
-        console.log(err);
-        const error = new HttpError("Fetching data failed, please try again later.", 500);
-        return next(error);
-    }
+  try {
+    const data = await Data.findAll();
+    res.json({ data });
+  } catch (err) {
+    console.log(err);
+    const error = new HttpError(
+      "Fetching data failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
 };
 
 exports.createData = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return next(
-            new HttpError(
-                errors.array()[0].msg ||
-                'Invalid inputs passed, please check your data',
-                422
-            )
-        );
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError(
+        errors.array()[0].msg ||
+          "Invalid inputs passed, please check your data",
+        422
+      )
+    );
+  }
 
-    const { IP, IPDetails, Host, Source, Domain, Brand, Time, Country, ISP, VPN, New, Archive } = req.body;
+  const {
+    IP,
+    IPDetails,
+    Host,
+    Source,
+    Domain,
+    Brand,
+    Time,
+    Country,
+    ISP,
+    VPN,
+    New,
+    Archive,
+    owner,
+    ISPDomain,
+  } = req.body;
 
-    try {
+  try {
+    const data = await fetch(
+      `https://restcountries.com/v3.1/name/${Country}?fields=flag,name`
+    ).then((data) => data.json());
 
-        const newData = await Data.create({ IP, IPDetails, Host, Source, Domain, Brand, Time, Country, ISP, VPN, New, Archive });
-        res.status(201).json({ dataId: newData.ID, IP: newData.IP, IPDetails: newData.IPDetails, Host: newData.Host, Source: newData.Source, Domain: newData.Domain, Brand: newData.Brand, Time: newData.Time, Country: newData.Country, ISP: newData.ISP, VPN: newData.VPN, New: newData.New, Archive: newData.Archive });
-    } catch (err) {
-        console.log(err);
-        const error = new HttpError(
-            'Registering data failed, please try again',
-            500
-        );
-        return next(error);
-    }
+    const newData = await Data.create({
+      IP,
+      IPDetails,
+      Host,
+      owner,
+      Source,
+      Domain,
+      Brand,
+      Time,
+      flag: data.flag,
+      Country: data.name.common,
+      ISP,
+      VPN,
+      New,
+      Archive,
+    });
+    res
+      .status(201)
+      .json({
+        dataId: newData.ID,
+        IP: newData.IP,
+        IPDetails: newData.IPDetails,
+        Host: newData.Host,
+        Source: newData.Source,
+        Domain: newData.Domain,
+        Brand: newData.Brand,
+        Time: newData.Time,
+        Country: newData.Country,
+        ISP: newData.ISP,
+        VPN: newData.VPN,
+        New: newData.New,
+        Archive: newData.Archive,
+      });
+  } catch (err) {
+    console.log(err);
+    const error = new HttpError(
+      "Registering data failed, please try again",
+      500
+    );
+    return next(error);
+  }
 };
 
 exports.deleteData = async (req, res, next) => {
-    const dataId = req.params.did;
+  const dataId = req.params.did;
 
-    let data;
-    try {
-        data = await Data.findByPk(dataId);
-    } catch (err) {
-        const error = new HttpError(
-            "Something went wrong, could not delete data.",
-            500
-        );
-        return next(error);
-    }
+  let data;
+  try {
+    data = await Data.findByPk(dataId);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete data.",
+      500
+    );
+    return next(error);
+  }
 
-    if (!data) {
-        const error = new HttpError("Could not find this data", 404);
-        return next(error);
-    }
+  if (!data) {
+    const error = new HttpError("Could not find this data", 404);
+    return next(error);
+  }
 
-    try {
-        await data.destroy();
-    } catch (err) {
-        const error = new HttpError(
-            "Something went wrong, could not delete data.",
-            500
-        );
-        return next(error);
-    }
+  try {
+    await data.destroy();
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, could not delete data.",
+      500
+    );
+    return next(error);
+  }
 
-    res.status(200).json({ message: "Deleted data." });
+  res.status(200).json({ message: "Deleted data." });
 };
--- models/Data.js
+++ models/Data.js
@@ -3,111 +3,123 @@ const sequelize = require("../config/connectionPool");
 
 // define data schema
 const Data = sequelize.define(
-    "Data",
-    {
-        ID: {
-            type: Sequelize.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false,
-        },
-        IP: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        IPDetails: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        Host: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        Source: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        Domain: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        Brand: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        Country: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        ISP: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        VPN: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        New: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        Archive: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        created_at: {
-            type: Sequelize.DATE,
-            defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-            allowNull: false,
-        },
-        updated_at: {
-            type: Sequelize.DATE,
-            defaultValue: Sequelize.literal(
-                "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
-            ),
-            allowNull: false,
-        },
+  "Data",
+  {
+    ID: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
     },
-    {
-        indexes: [
-            {
-                name: "idx_ip",
-                fields: ["IP"],
-            },
-            {
-                name: "idx_ipdetails",
-                fields: ["IPDetails"],
-            },
-            {
-                name: "idx_host",
-                fields: ["Host"],
-            },
-            {
-                name: "idx_brand",
-                fields: ["Brand"],
-            },
-            {
-                name: "idx_country",
-                fields: ["Country"],
-            },
-            {
-                name: "idx_isp",
-                fields: ["ISP"],
-            },
-            {
-                name: "idx_email",
-                fields: ["Domain"],
-            },
-        ],
-    }
+    IP: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    IPDetails: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    Host: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    Source: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    Domain: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    owner: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    Brand: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    Country: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    countryFlag: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    ISP: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    ISPDomain: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    VPN: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    New: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    Archive: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.literal(
+        "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+      ),
+      allowNull: false,
+    },
+  },
+  {
+    indexes: [
+      {
+        name: "idx_ip",
+        fields: ["IP"],
+      },
+      {
+        name: "idx_ipdetails",
+        fields: ["IPDetails"],
+      },
+      {
+        name: "idx_host",
+        fields: ["Host"],
+      },
+      {
+        name: "idx_brand",
+        fields: ["Brand"],
+      },
+      {
+        name: "idx_country",
+        fields: ["Country"],
+      },
+      {
+        name: "idx_isp",
+        fields: ["ISP"],
+      },
+      {
+        name: "idx_email",
+        fields: ["Domain"],
+      },
+    ],
+  }
 );
 
 Data.sync()
-    .then(() => {
-        console.log("Data table created successfully");
-    })
-    .catch((error) => {
-        console.error("Error creating Data table:", error);
-    });
+  .then(() => {
+    console.log("Data table created successfully");
+  })
+  .catch((error) => {
+    console.error("Error creating Data table:", error);
+  });
 
 module.exports = Data;
